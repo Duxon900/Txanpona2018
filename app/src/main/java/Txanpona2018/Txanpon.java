0c151e3a4c2aa72239f684aa59935f38b23a552d
Duxon900 <quintanojon@gmail.com>
2021-01-05 22:08:45 +0100

    taula working

---
+++ app/src/main/java/Txanpona2018/Txanpon.java
@@ -0,0 +1,49 @@
+package Txanpona2018;
+
+public class Txanpon {
+    private int id;
+    private String data;
+    private double balioa;
+    private String mota;
+    private float bolumena;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public double getBalioa() {
+        return balioa;
+    }
+
+    public void setBalioa(double balioa) {
+        this.balioa = balioa;
+    }
+
+    public String getMota() {
+        return mota;
+    }
+
+    public void setMota(String mota) {
+        this.mota = mota;
+    }
+
+    public float getBolumena() {
+        return bolumena;
+    }
+
+    public void setBolumena(float bolumena) {
+        this.bolumena = bolumena;
+    }
+}
--- app/src/main/java/Txanpona2018/control/ui/MainUIKud.java
+++ app/src/main/java/Txanpona2018/control/ui/MainUIKud.java
@@ -1,9 +1,25 @@
 package Txanpona2018.control.ui;
 
+import Txanpona2018.Txanpon;
+import Txanpona2018.control.db.DBKudeatzaile;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class MainUIKud {
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class MainUIKud implements Initializable {
+
+    @FXML
+    private TableView<Txanpon> taula;
+
 
     @FXML
     private TableColumn<?, ?> zutID;
@@ -23,4 +39,37 @@ public class MainUIKud {
     @FXML
     private TableColumn<?, ?> zutPortaera;
 
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ResultSet resultSet=DBKudeatzaile.getInstantzia().execSQL("SELECT * FROM txanponak");
+        try {
+            datuakSartu(resultSet);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        zutID.setCellValueFactory(new PropertyValueFactory<>("ID"));
+        zutTxanpon.setCellValueFactory(new PropertyValueFactory<>("Txanpon"));
+        zutNoiz.setCellValueFactory(new PropertyValueFactory<>("Noiz"));
+        zutZenbat.setCellValueFactory(new PropertyValueFactory<>("Zenbat"));
+        zutBolumena.setCellValueFactory(new PropertyValueFactory<>("Bolumena"));
+        zutPortaera.setCellValueFactory(new PropertyValueFactory<>("Portaera"));
+
+    }
+
+    private void datuakSartu(ResultSet resultSet) throws SQLException {
+        ObservableList<Txanpon> emaitza= FXCollections.observableArrayList();
+
+        while(resultSet.next()){
+            Txanpon txanpon=new Txanpon();
+            txanpon.setId(resultSet.getInt("id"));
+            txanpon.setData(resultSet.getString("data"));
+            txanpon.setBalioa(resultSet.getDouble("balioa"));
+            txanpon.setMota(resultSet.getString("mota"));
+            txanpon.setBolumena(resultSet.getFloat("bolumena"));
+            emaitza.add(txanpon);
+        }
+        taula.setItems(emaitza);
+    }
 }
